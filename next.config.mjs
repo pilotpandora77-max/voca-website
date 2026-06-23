7910c4beb9c2b23828d82fe7ff1e6e3b6aac0762
Quantorix <quantorix@Quantorixs-MacBook-Air.local>
2026-06-24 00:43:07 +0800

    Proxy vocamongolia.cc to EAS hosted app

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        destination: 'https://temuujin77-voca.expo.app/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
